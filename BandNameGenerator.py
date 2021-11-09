2d4173314462adee6495e56f0876e22ee2ff90fd
Liz Hall <elizabethvhall@gmail.com>
2021-11-08 23:32:06 -0500

    Day 1 commit

---
+++ BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator!")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of a pet you had?\n")
+print("Your band name is " + city + " "+ pet)
